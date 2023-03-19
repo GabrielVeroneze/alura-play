ffec126284c5414ba53cfe0e3d0c982e396c3e98
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-18 22:31:11 -0300

    :sparkles: feat: Faz uma requisição POST enviando um conjunto de dados

--- assets/js/conectaApi.js
+++ assets/js/conectaApi.js
@@ -7,8 +7,38 @@ async function listaVideos() {
     return conexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })    
+    }
+
+    const conexao = await fetch('http://localhost:3000/videos', options)
+    const conexaoConvertida = await conexao.json()
+
+    return conexaoConvertida
+}
+
+criaVideo(
+    "Linguagens e ferramentas usadas em Ciência de Dados...",
+    "2,5",
+    "https://www.youtube.com/embed/h83e1aAM5xQ",
+    "https://github.com/MonicaHillman/aluraplay-requisicoes/blob/main/img/logo.png?raw=true"
+)
+
 // Ao exportar a função "listaVideos" como parte de um objeto, outras partes do código podem importar e usar essa função. 
 // Quando outras partes do código precisarem acessar a função "listaVideos" podem importar o objeto "conectaApi" e chamar o método "listaVideos". Sintaxe: "conectaApi.listaVideos()".
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
