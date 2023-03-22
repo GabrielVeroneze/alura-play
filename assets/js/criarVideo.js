22675bb2d1a85b1d825442ecbb0bc1753b3a1db3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-21 21:42:07 -0300

    :sparkles: feat: Cria o vídeo e envia ao servidor

--- assets/js/criarVideo.js
+++ assets/js/criarVideo.js
@@ -1,14 +1,24 @@
+import { conectaApi } from "./conectaApi.js"
+
 const formulario = document.querySelector('[data-formulario]')
 
-function criaVideos(evento) {
+async function criaVideos(evento) {
     //  Impede o comportamento padrão do 'submit', que é enviar o formulário e recarregar a página, permitindo que uma ação personalizada seja realizada
     evento.preventDefault()
 
-    const url = document.querySelector('[data-url]').value
     const titulo = document.querySelector('[data-titulo]').value
     const imagem = document.querySelector('[data-imagem]').value
+
+    // Substitui uma parte da string por outra, modificando a url para o formato de incorporação (embed), que pode ser usado para exibir o vídeo em uma página web
+    const url = document.querySelector('[data-url]').value
+    const urlEmbed = url.replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+
     // Gera numero aleatório entre 0 e 100 convertido em string
     const descricao = Math.floor(Math.random() * 100).toString()
+
+    await conectaApi.enviaVideos(titulo, descricao, urlEmbed, imagem)
+
+    window.location.href = "/pages/envio-concluido.html"
 }
 
 formulario.addEventListener('submit', evento => criaVideos(evento))
