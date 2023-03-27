7feb95ac5a50af83f1ddcc95605cf6faae796d6a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-26 23:05:26 -0300

    :sparkles: feat: Pega o vídeo pesquisado e o exibe

---
+++ assets/js/pesquisarVideo.js
@@ -0,0 +1,19 @@
+import { conectaApi } from "./conectaApi.js"
+import { criaItem }  from "./mostrarVideos.js"
+
+async function pesquisarVideo(evento) {
+    evento.preventDefault()
+    
+    const dadosDePesquisa = document.querySelector('[data-pesquisa]').value
+    const pesquisa = await conectaApi.pesquisaVideos(dadosDePesquisa)
+
+    const elementoLista = document.querySelector('[data-lista]')
+
+    pesquisa.forEach(video => {
+        elementoLista.appendChild(criaItem(video.titulo, video.descricao, video.url, video.imagem))
+    })
+}
+
+const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]')
+
+botaoDePesquisa.addEventListener('click', evento => pesquisarVideo(evento))
