ad42803c5a219833c4000cf4723b90c40d4d793b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-03-27 18:19:14 -0300

    :bulb: docs: Comentários sobre a pesquisa de videos

--- assets/js/pesquisarVideo.js
+++ assets/js/pesquisarVideo.js
@@ -9,10 +9,13 @@ async function pesquisarVideo(evento) {
 
     const elementoLista = document.querySelector('[data-lista]')
 
+    // O loop é executado enquanto 'elementoLista' tiver um primeiro elemento filho, quanto não houver elemento filho o loop é interrompido
     while (elementoLista.firstElementChild) {
+        // o método removeChild() é chamado passando o primeiro elemento filho de 'elementoLista' como argumento, removendo ele
         elementoLista.removeChild(elementoLista.firstElementChild)
     }
 
+    // Para cada dado retornado da pesquisa, cria o card de video, e adiciona na lista de videos
     pesquisa.forEach(video => {
         elementoLista.appendChild(criaItem(video.titulo, video.descricao, video.url, video.imagem))
     })
